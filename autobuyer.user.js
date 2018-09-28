8c851e9a525f2c3a02da4fd627f33a9d06415069
Unsworth94 <benunsworth_4242@live.com>
2018-09-28 11:42:10 +0100

    Update autobuyer.user.js

--- autobuyer.user.js
+++ autobuyer.user.js
@@ -2,6 +2,7 @@
 // @name         FUT19 Autobuyer
 // @namespace    http://tampermonkey.net/
 // @version      0.1
+// @updateURL    https://github.com/Unsworth94/fut19-web-app/raw/master/autobuyer.user.js
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.easports.com/fifa/ultimate-team/web-app/*
