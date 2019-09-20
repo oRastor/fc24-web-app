8d99329c3c7e16fdf665eef3687e77e4583723bf
Orest Budzhak <doom4eg@gmail.com>
2019-09-20 13:33:03 +0300

    Update autobuyer.user.js

--- autobuyer.user.js
+++ autobuyer.user.js
@@ -6,6 +6,7 @@
 // @description  try to take over the world!
 // @author       Rastor
 // @match        https://www.easports.com/uk/fifa/ultimate-team/web-app/*
+// @match        https://www.easports.com/fifa/ultimate-team/web-app/*
 // @grant        none
 // ==/UserScript==
 
