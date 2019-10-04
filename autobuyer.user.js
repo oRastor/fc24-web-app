fdd08c02958997fc4db6f46212311bb8250e3896
Orest Budzhak <doom4eg@gmail.com>
2019-10-04 19:35:50 +0300

    Update autobuyer.user.js

--- autobuyer.user.js
+++ autobuyer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FUT20 Autobuyer
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @updateURL    https://github.com/oRastor/fut20-web-app/raw/master/autobuyer.user.js
 // @description  try to take over the world!
 // @author       Rastor
